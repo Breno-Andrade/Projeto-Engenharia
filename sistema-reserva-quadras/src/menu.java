2c42ce9aec2c179847798ee491b0553087d05df1
Lucas Vinícius <lucassdeveloperr@gmail.com>
2022-04-07 19:20:10 -0300

    estrutura do menu
    estrutura básica

--- sistema-reserva-quadras/src/menu.java
+++ sistema-reserva-quadras/src/menu.java
@@ -1,3 +1,27 @@
+import javax.swing.JOptionPane;
+
 public class menu {
-    
+    public static void main(String[] args) {
+        String aux = JOptionPane.showInputDialog(null, "Menu principal \n ---------------------\n 1 - Agendamento\n 2 - Gestão de quadras\n 3 - Gestão de usuários\n 4 - Estatísticas do clube\n 5 - Sair");  
+        int op = Integer.parseInt(aux);
+        switch (op)) {
+            case 1:
+            
+                break;
+            case 2:
+            
+                break;
+            case 3: 
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+                
+                break;
+            default:
+               
+        }
+    }
 }
