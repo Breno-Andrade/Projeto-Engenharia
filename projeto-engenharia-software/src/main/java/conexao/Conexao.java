b78c7df9af8857b8007d458e59448cb95e12da2f
Rodrigo Kendy Kinoshita <99501065+RoKinoshita@users.noreply.github.com>
2022-05-26 20:29:17 -0300

    Adicionado Loger na conexão

--- projeto-engenharia-software/src/main/java/conexao/Conexao.java
+++ projeto-engenharia-software/src/main/java/conexao/Conexao.java
@@ -2,11 +2,12 @@ package conexao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class Conexao {
 
     public static Connection conectar(){
-        final String Url_SQL = "jdbc:postgresql://192.168.15.12:5432/reserva";
+        final String Url_SQL = "jdbc:postgresql://localhost:5432/reserva";
         final String User_SQL = "postgres";
 	    final String Password_SQL = "017017";
       
@@ -14,21 +15,22 @@ public class Conexao {
             Connection conexao = DriverManager.getConnection(Url_SQL, User_SQL, Password_SQL); 
             return conexao;
         } catch (final SQLException e) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, e);
             e.printStackTrace();
         }
         return null;
     }
 
     public static Connection desconectar() {
-        final String Url_SQL = "jdbc:postgresql://192.168.15.12:5432/reserva";
-        final String User_SQL = "postgres";
-	    final String Password_SQL = "017017";
-
         try {
+            final String Url_SQL = "jdbc:postgresql://localhost:5432/reserva";
+            final String User_SQL = "postgres";
+            final String Password_SQL = "017017";
             Connection conexao = DriverManager.getConnection(Url_SQL, User_SQL, Password_SQL); 
             conexao.close();
             return conexao;
         } catch (final SQLException e) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, e);
             e.printStackTrace();
         }
         return null;  
