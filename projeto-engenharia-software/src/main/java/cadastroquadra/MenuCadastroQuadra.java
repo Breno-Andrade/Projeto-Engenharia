b76524a977d905575b5ec60d5a70af316397d70f
Rodrigo Kendy Kinoshita <99501065+RoKinoshita@users.noreply.github.com>
2022-06-01 23:47:23 -0300

    Modificação cadastro quadra

--- projeto-engenharia-software/src/main/java/cadastroquadra/MenuCadastroQuadra.java
+++ projeto-engenharia-software/src/main/java/cadastroquadra/MenuCadastroQuadra.java
@@ -14,19 +14,25 @@ public class MenuCadastroQuadra{
                 
             String aux = JOptionPane.showInputDialog("Digite o Número: ");
             int numero = Integer.parseInt(aux);
-            
+
             String[] valores  = {"Saibro", "Tenis rápido","Beath tenis"};
-            tipo = (String)JOptionPane.showInputDialog(null, "Escolha um tipo de quadra", "quadra", JOptionPane.QUESTION_MESSAGE, null, valores, valores[0]);
+            tipo = (String)JOptionPane.showInputDialog(null, "Escolha um tipo de quadra", "quadra", 
+            JOptionPane.QUESTION_MESSAGE, null, valores, valores[0]);
            
             String[] valores2  = {"SIM", "NÃO",};
-            coberta = (String)JOptionPane.showInputDialog(null, "Possui cobertura? ", "quadra", JOptionPane.QUESTION_MESSAGE, null, valores2, valores2[0]);
+            coberta = (String)JOptionPane.showInputDialog(null, "Possui cobertura? ", "quadra", 
+            JOptionPane.QUESTION_MESSAGE, null, valores2, valores2[0]);
             
             String[] valores3  = {"SIM", "NÃO",};
-            arquibancada = (String)JOptionPane.showInputDialog(null, "Possui arquibancada? ", "quadra", JOptionPane.QUESTION_MESSAGE, null, valores3, valores3[0]);
+            arquibancada = (String)JOptionPane.showInputDialog(null, "Possui arquibancada? ", "quadra", 
+            JOptionPane.QUESTION_MESSAGE, null, valores3, valores3[0]);
             
             String[] valores4  = {"SIM", "NÃO",};
-            banco = (String)JOptionPane.showInputDialog(null, "Possui bancos para os jogadores? ", "quadra", JOptionPane.QUESTION_MESSAGE, null, valores4, valores4[0]);
+            banco = (String)JOptionPane.showInputDialog(null, "Possui bancos para os jogadores? ", "quadra", 
+            JOptionPane.QUESTION_MESSAGE, null, valores4, valores4[0]);
         
+  
+
       Quadra q = new Quadra();
       q.setNumero(numero);
       q.setTipo(tipo);	
