039b50ffd81a652447aed034af368ae26bad8621
Breno Andrade <breno.silva15@fatec.sp.gov.br>
2022-06-02 17:00:19 -0300

    feat(cadastraUsuario): criando a nova funcionalidade

---
+++ projeto-engenharia-software/src/main/java/CadastraUsuario/CadastraUsuario.java
@@ -0,0 +1,11 @@
+package CadastraUsuario;
+
+public class CadastraUsuario {
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String usuario;
+    private String senha;
+    
+    
+}
